de38d2e8b265f291180c936ddea7d5139e8dfa83
Colunga-D <xkould@gmail.com>
2023-12-19 09:53:32 +0100

    films API

---
+++ src/test/java/com/example/API/Films/ApiFilmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.API.Films;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFilmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
